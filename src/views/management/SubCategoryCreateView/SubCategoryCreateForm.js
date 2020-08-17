ba8db24fb39c5e801e2e50cf23076b0b6f8f0c27
nitheshurao <31339538+nitheshurao@users.noreply.github.com>
2020-08-17 22:27:31 +0530

    create new sub category chagess

--- src/views/management/SubCategoryCreateView/SubCategoryCreateForm.js
+++ src/views/management/SubCategoryCreateView/SubCategoryCreateForm.js
@@ -1,85 +1,225 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+/* eslint-disable max-len */
+import React, { useState } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 import clsx from 'clsx';
+import PropTypes from 'prop-types';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import { useHistory } from 'react-router';
+
 import * as Yup from 'yup';
 import { Formik } from 'formik';
+import QuillEditor from 'src/components/QuillEditor';
+import FilesDropzone from 'src/components/FilesDropzone';
+
 import { useSnackbar } from 'notistack';
 import {
   Box,
   Button,
   Card,
   CardContent,
+  CardHeader,
+  Checkbox,
+  Divider,
+  FormControlLabel,
+  FormHelperText,
   Grid,
-  Switch,
+  Paper,
   TextField,
   Typography,
   makeStyles
+  
+  
 } from '@material-ui/core';
-import wait from 'src/utils/wait';
+import {
+  Image as ImageIcon,
+  Edit as EditIcon,
+  ArrowRight as ArrowRightIcon,
+  Search as SearchIcon
+} from 'react-feather';
+import Label from 'src/components/Label';
 
 const categoryOptions = [
   {
-    id: 'all',
-    name: 'All'
+    id: 'shirts',
+    name: 'Shirts'
   },
   {
-    id: 'dress',
-    name: 'Dress'
+    id: 'phones',
+    name: 'Phones'
   },
   {
-    id: 'jewelry',
-    name: 'Jewelry'
-  },
-  {
-    id: 'blouse',
-    name: 'Blouse'
-  },
-  {
-    id: 'beauty',
-    name: 'Beauty'
+    id: 'cars',
+    name: 'Cars'
   }
 ];
 
-const useStyles = makeStyles(() => ({
-  root: {}
+
+
+function applyFilters(products, query) {
+  return products.filter((product) => {
+    let matches = true;
+
+    if (query && !product.name.toLowerCase().includes(query.toLowerCase())) {
+      matches = false;
+    }
+
+   
+  
+    return matches;
+  });
+}
+
+function applyPagination(customers, page, limit) {
+  return customers.slice(page * limit, page * limit + limit);
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {},
+  bulkOperations: {
+    position: 'relative'
+  },
+  bulkActions: {
+    paddingLeft: 4,
+    paddingRight: 4,
+    marginTop: 6,
+    position: 'absolute',
+    width: '100%',
+    zIndex: 2,
+    backgroundColor: theme.palette.background.default
+  },
+  bulkAction: {
+    marginLeft: theme.spacing(2)
+  },
+  queryField: {
+    width: 500
+  },
+  categoryField: {
+    flexBasis: 200
+  },
+  root: {},
+  editor: {
+    '& .ql-editor': {
+      height: 400
+    }
+  },
+  
+  
 }));
 
-function SubCategoryCreateForm({
-  className,
-  customer,
-  ...rest
-}) {
+function Results({ className, products, ...rest }) {
   const classes = useStyles();
-  const { enqueueSnackbar } = useSnackbar();
+  const [selectedProducts, setSelectedProducts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [query, setQuery] = useState('');
+  
+  const [filters, setFilters] = useState({
+    category: null,
+   
+  });
 
+  const handleQueryChange = (event) => {
+    event.persist();
+    setQuery(event.target.value);
+  };
+
+  const handleCategoryChange = (event) => {
+    event.persist();
+
+    let value = null;
+
+    if (event.target.value !== 'all') {
+      value = event.target.value;
+    }
+
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      category: value
+    }));
+  };
+
+  
+  
+
+  
+
+
+  const handleSelectAllProducts = (event) => {
+    setSelectedProducts(event.target.checked
+      ? products.map((product) => product.id)
+      : []);
+  };
+
+  const handleSelectOneProduct = (event, productId) => {
+    if (!selectedProducts.includes(productId)) {
+      setSelectedProducts((prevSelected) => [...prevSelected, productId]);
+    } else {
+      setSelectedProducts((prevSelected) => prevSelected.filter((id) => id !== productId));
+    }
+  };
+
+  const handlePageChange = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleLimitChange = (event) => {
+    setLimit(event.target.value);
+  };
+
+  // Usually query is done on backend with indexing solutions
+  const filteredProducts = applyFilters(products, query, filters);
+  const paginatedProducts = applyPagination(filteredProducts, page, limit);
+  const enableBulkOperations = selectedProducts.length > 0;
+  const selectedSomeProducts = selectedProducts.length > 0 && selectedProducts.length < products.length;
+  const selectedAllProducts = selectedProducts.length === products.length;
+  
+  const history = useHistory();
+  const { enqueueSnackbar } = useSnackbar();
   return (
+
     <Formik
       initialValues={{
-        
+        category: '',
+        subcategory: '',
+        description: '',
+        images: [],
+        includesTaxes: false,
+        isTaxable: false,
+        name: '',
+        price: '',
+        productCode: '',
+        productSku: '',
+        salePrice: ''
       }}
       validationSchema={Yup.object().shape({
-        subcategoryName: Yup.string().max(255),
-     
+        category: Yup.string().max(255),
+        subcategory: Yup.string().max(255),
+        description: Yup.string().max(5000),
+        images: Yup.array(),
+        includesTaxes: Yup.bool().required(),
+        isTaxable: Yup.bool().required(),
+        name: Yup.string().max(255).required(),
+        price: Yup.number().min(0).required(),
+        productCode: Yup.string().max(255),
+        productSku: Yup.string().max(255),
+        salePrice: Yup.number().min(0)
       })}
       onSubmit={async (values, {
-        resetForm,
         setErrors,
         setStatus,
         setSubmitting
       }) => {
         try {
-          // Make API request
-          await wait(500);
-          resetForm();
+          // Do api call
           setStatus({ success: true });
           setSubmitting(false);
-          enqueueSnackbar('User Added', {
-            variant: 'success',
-            action: <Button>See all</Button>
+          enqueueSnackbar('Product Created', {
+            variant: 'success'
           });
-        } catch (error) {
+          history.push('/app/products');
+        } catch (err) {
+          setErrors({ submit: err.message });
           setStatus({ success: false });
-          setErrors({ submit: error.message });
           setSubmitting(false);
         }
       }}
@@ -90,102 +230,150 @@ function SubCategoryCreateForm({
         handleChange,
         handleSubmit,
         isSubmitting,
+        setFieldValue,
         touched,
         values
       }) => (
         <form
-          className={clsx(classes.root, className)}
           onSubmit={handleSubmit}
+          className={clsx(classes.root, className)}
           {...rest}
         >
-           <Box p={2}>
-      
-      <Box
-        mt={3}
-        display="flex"
-        alignItems="center"
-      >
-        <TextField
-          className={classes.categoryField}
-          label="Category"
-          name="category"
-          onChange={  (event) => {
-            event.persist();
-        
-            let value = null;
-        
-            if (event.target.value !== 'all') {
-              value = event.target.value;
-            }
-        
-          
-          }
-        }
-          select
-          SelectProps={{ native: true }}
-          value={'a','asa','asas'}
-          variant="outlined"
+    <Card
+      className={clsx(classes.root, className)}
+      {...rest}
+    >
+      <Box p={2}>
+        <Box
+          display="flex"
+          alignItems="center"
         >
-          {categoryOptions.map((categoryOption) => (
-            <option
-              key={categoryOption.id}
-              value={categoryOption.id}
-            >
-              {categoryOption.name}
-            </option>
-          ))}
-        </TextField>
-      
-      
-      </Box>
-    </Box>
-          <Card>
+          
+          
+        </Box>
+       
+         
+          <Grid
+            container
+            spacing={3}
+          >
             
-            <CardContent>
-            <Grid
-                container
-                spacing={1}
+              <Card>
+                <CardContent>
+                <TextField
+            className={classes.categoryField}
+            label="Category"
+            name="category"
+            onChange={handleCategoryChange}
+            select
+            SelectProps={{ native: true }}
+            value={filters.category || 'all'}
+            variant="outlined"
+          >
+            {categoryOptions.map((categoryOption) => (
+              <option
+                key={categoryOption.id}
+                value={categoryOption.id}
               >
-            <Grid
-                  item
-                  md={6}
-                  xs={12}
-                > 
-              <TextField
-              label =" New Category"
-              name="subcategoryName"
-              required
-              onBlur={handleBlur}
-              onChange={handleChange}
-              value={values.categoryName}
-              variant="outlined"
-              />
-              </Grid>
-              <Box mt={2}
-                item
-                md={6}
-                xs={12}>
-                <Button
-                  variant="contained"
-                  color="secondary"
-                  type="submit"
-                  disabled={isSubmitting}
-                >
-                  Add User
-                </Button>
+                {categoryOption.name}
+              </option>
+            ))}
+          </TextField>
+          
+        
+                  <TextField
+                    error={Boolean(touched.name && errors.name)}
+                    fullWidth
+                    helperText={touched.name && errors.name}
+                    label="category Name"
+                    name="name"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.name}
+                    variant="outlined"
+                  />
+                   
+                  <Box
+                    mt={3}
+                    mb={1}
+                  >
+
+                    
+                    <Typography
+                      variant="subtitle2"
+                      color="textSecondary"
+                    >
+                      Description
+                    </Typography>
+                  </Box>
+                  <Paper variant="outlined">
+                    <QuillEditor
+                      className={classes.editor}
+                      value={values.description}
+                      onChange={(value) => setFieldValue('description', value)}
+                    />
+                  </Paper>
+                  {(touched.description && errors.description) && (
+                    <Box mt={2}>
+                      <FormHelperText error>
+                        {errors.description}
+                      </FormHelperText>
+                    </Box>
+                  )}
+                </CardContent>
+              </Card>
+              <Box mt={3}>
+                <Card>
+                  <CardHeader title="Upload Images" />
+                  <Divider />
+                  <CardContent>
+                    <FilesDropzone />
+                  </CardContent>
+                </Card>
               </Box>
-              </Grid>
-            </CardContent>
-          </Card>
-        </form>
-      )}
+           
+            </Grid>
+           
+     
+          {errors.submit && (
+            <Box mt={3}>
+              <FormHelperText error>
+                {errors.submit}
+              </FormHelperText>
+            </Box>
+          )}
+          <Box mt={2}>
+            <Button
+              color="secondary"
+              variant="contained"
+              type="submit"
+              disabled={isSubmitting}
+            >
+              Create Category
+            </Button>
+          </Box>
+        
+
+    
+   
+        </Box>
+    
+     
+     
+    </Card>
+    </form>)}
     </Formik>
   );
 }
 
-SubCategoryCreateForm.propTypes = {
+
+Results.propTypes = {
   className: PropTypes.string,
-  customer: PropTypes.object.isRequired
+  products: PropTypes.array
 };
 
-export default SubCategoryCreateForm;
+Results.defaultProps = {
+  products: []
+};
+
+export default Results;
