6f2cfde34a8b0e168e4abbf6e44db53a7df35fb9
nitheshurao <31339538+nitheshurao@users.noreply.github.com>
2020-08-27 21:40:52 +0530

    cheges

--- src/views/management/CategoryCreateView/CategoryCreateForm.js
+++ src/views/management/CategoryCreateView/CategoryCreateForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useHistory } from 'react-router';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 import clsx from 'clsx';
 import * as Yup from 'yup';
 import axios from 'axios';
@@ -25,8 +26,6 @@ import {
 import QuillEditor from 'src/components/QuillEditor';
 import FilesDropzone from 'src/components/FilesDropzone';
 
-
-
 const useStyles = makeStyles(() => ({
   root: {},
   editor: {
@@ -36,11 +35,11 @@ const useStyles = makeStyles(() => ({
   }
 }));
 
-function ProductCreateForm({ className, ...rest }) {
+function ProductCreateForm({ className, onSubmitSuccess, ...rest }) {
   const classes = useStyles();
   const history = useHistory();
   const { enqueueSnackbar } = useSnackbar();
-
+  const dispatch = useDispatch();
   return (
     <Formik
       initialValues={{
@@ -63,36 +62,32 @@ function ProductCreateForm({ className, ...rest }) {
         images: Yup.array(),
         includesTaxes: Yup.bool().required(),
         isTaxable: Yup.bool().required(),
-        name: Yup.string().max(255).required(),
-       
+        name: Yup.string()
+          .max(255)
+          .required()
       })}
-      onSubmit={async (values ={
-        name:this.name,
-        
-
-
-      } , {
-        setErrors,
-        setStatus,
-        setSubmitting 
-      }) => {
+      onSubmit={async (values, { setErrors, setStatus, setSubmitting }) => {
         try {
-          console.log('...try...')
-          axios
-          .post('http://15.207.7.54:8080/category/register')
-          .then((res) => {console.log(' ...res');
-        console.log(res.data)  } )
+          console.log('...try...');
+          await dispatch((values.name, values.description));
+          onSubmitSuccess();
+          console.log('...try...');
+          
+          axios.post('http://15.207.7.54:8080/category/register').then(res => {
+            console.log(' ...res');
+            console.log(res.data);
+          });
           // Do api call
           setStatus({ success: true });
           setSubmitting(false);
           enqueueSnackbar('Product Created', {
             variant: 'success'
           });
-        
-          history.push('/app/management/CategoryCreateView')
-          .then((res) => {
-console.log(' ...res');
-              console.log(res.data) } )
+
+          history.push(' http://15.207.7.54:8080/sub-category/register').then(res => {
+            console.log(' ...res');
+            console.log(res.data);
+          });
         } catch (err) {
           setErrors({ submit: err.message });
           setStatus({ success: false });
@@ -104,7 +99,7 @@ console.log(' ...res');
         errors,
         handleBlur,
         handleChange,
-        handleSubmit ,
+        handleSubmit,
         isSubmitting,
         setFieldValue,
         touched,
@@ -115,15 +110,8 @@ console.log(' ...res');
           className={clsx(classes.root, className)}
           {...rest}
         >
-          <Grid
-            container
-            spacing={3}
-          >
-            <Grid
-              item
-              xs={12}
-              lg={8}
-            >
+          <Grid container spacing={3}>
+            <Grid item xs={12} lg={8}>
               <Card>
                 <CardContent>
                   <TextField
@@ -137,17 +125,9 @@ console.log(' ...res');
                     value={values.name}
                     variant="outlined"
                   />
-                   
-                  <Box
-                    mt={3}
-                    mb={1}
-                  >
 
-                    
-                    <Typography
-                      variant="subtitle2"
-                      color="textSecondary"
-                    >
+                  <Box mt={3} mb={1}>
+                    <Typography variant="subtitle2" color="textSecondary">
                       Description
                     </Typography>
                   </Box>
@@ -155,10 +135,10 @@ console.log(' ...res');
                     <QuillEditor
                       className={classes.editor}
                       value={values.description}
-                      onChange={(value) => setFieldValue('description', value)}
+                      onChange={value => setFieldValue('description', value)}
                     />
                   </Paper>
-                  {(touched.description && errors.description) && (
+                  {touched.description && errors.description && (
                     <Box mt={2}>
                       <FormHelperText error>
                         {errors.description}
@@ -176,15 +156,11 @@ console.log(' ...res');
                   </CardContent>
                 </Card>
               </Box>
-           
             </Grid>
-            
           </Grid>
           {errors.submit && (
             <Box mt={3}>
-              <FormHelperText error>
-                {errors.submit}
-              </FormHelperText>
+              <FormHelperText error>{errors.submit}</FormHelperText>
             </Box>
           )}
           <Box mt={2}>
@@ -204,7 +180,12 @@ console.log(' ...res');
 }
 
 ProductCreateForm.propTypes = {
-  className: PropTypes.string
+  className: PropTypes.string,
+  onSubmitSuccess: PropTypes.func
+};
+
+ProductCreateForm.defaultProps = {
+  onSubmitSuccess: () => {}
 };
 
 export default ProductCreateForm;
