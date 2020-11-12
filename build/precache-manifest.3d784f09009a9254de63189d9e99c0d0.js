2f6a85407921c8914a893233f45663d46e947023
nithesh <nitheshurao@gmail.com>
2020-11-12 19:25:51 +0530

    changes APi

---
+++ build/precache-manifest.3d784f09009a9254de63189d9e99c0d0.js
@@ -0,0 +1,406 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "289eada89275d465dcb5652fbaf5b04e",
+    "url": "/BuildX-Admin-Panel/index.html"
+  },
+  {
+    "revision": "f275801cba0895fd1d2a",
+    "url": "/BuildX-Admin-Panel/static/css/16.b7fe165f.chunk.css"
+  },
+  {
+    "revision": "3be08019ee327d65e5ff",
+    "url": "/BuildX-Admin-Panel/static/css/21.fc1d31e8.chunk.css"
+  },
+  {
+    "revision": "e57ebb8ad1663e2d6cc5",
+    "url": "/BuildX-Admin-Panel/static/css/main.2b289f01.chunk.css"
+  },
+  {
+    "revision": "64c350833ced688ec7a3",
+    "url": "/BuildX-Admin-Panel/static/js/0.efad788c.chunk.js"
+  },
+  {
+    "revision": "54d061cfd1236b9c46a8",
+    "url": "/BuildX-Admin-Panel/static/js/1.ab1ad084.chunk.js"
+  },
+  {
+    "revision": "bd49b04565accd5b6f2624c86f8385ba",
+    "url": "/BuildX-Admin-Panel/static/js/1.ab1ad084.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c70e7734c5a893835e01",
+    "url": "/BuildX-Admin-Panel/static/js/10.ed913bbb.chunk.js"
+  },
+  {
+    "revision": "f178632245619757e144",
+    "url": "/BuildX-Admin-Panel/static/js/11.05e8d525.chunk.js"
+  },
+  {
+    "revision": "95b49e91880efaffb9cc",
+    "url": "/BuildX-Admin-Panel/static/js/12.60659535.chunk.js"
+  },
+  {
+    "revision": "0601d5cdaed461669035",
+    "url": "/BuildX-Admin-Panel/static/js/13.b4d34f9d.chunk.js"
+  },
+  {
+    "revision": "f275801cba0895fd1d2a",
+    "url": "/BuildX-Admin-Panel/static/js/16.26ed3bfa.chunk.js"
+  },
+  {
+    "revision": "3d990402b1039bf8e99572f8e1aee21f",
+    "url": "/BuildX-Admin-Panel/static/js/16.26ed3bfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34eb597d9cd76699fd9b",
+    "url": "/BuildX-Admin-Panel/static/js/17.9318b811.chunk.js"
+  },
+  {
+    "revision": "b07120534be78b82fdc28f52b06e3c48",
+    "url": "/BuildX-Admin-Panel/static/js/17.9318b811.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d4cc211b06d9fe62bb0",
+    "url": "/BuildX-Admin-Panel/static/js/18.558cab0b.chunk.js"
+  },
+  {
+    "revision": "3c548a488e94eaed40436091d8f7177d",
+    "url": "/BuildX-Admin-Panel/static/js/18.558cab0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5a92eb49be78c7258cb",
+    "url": "/BuildX-Admin-Panel/static/js/19.44acfcef.chunk.js"
+  },
+  {
+    "revision": "6e1b20bce150684153f6",
+    "url": "/BuildX-Admin-Panel/static/js/2.62e540b1.chunk.js"
+  },
+  {
+    "revision": "722a3d0affe970c8629c",
+    "url": "/BuildX-Admin-Panel/static/js/20.956fdb62.chunk.js"
+  },
+  {
+    "revision": "3be08019ee327d65e5ff",
+    "url": "/BuildX-Admin-Panel/static/js/21.6d133aa3.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/BuildX-Admin-Panel/static/js/21.6d133aa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1647e4e4e0a3c728e920",
+    "url": "/BuildX-Admin-Panel/static/js/22.ae72ad6e.chunk.js"
+  },
+  {
+    "revision": "c6855937c2e3f35075ae",
+    "url": "/BuildX-Admin-Panel/static/js/23.0bdf3b60.chunk.js"
+  },
+  {
+    "revision": "bf7973c05a157341d8ba",
+    "url": "/BuildX-Admin-Panel/static/js/24.085927ff.chunk.js"
+  },
+  {
+    "revision": "16850d129d7f7ba40fe7",
+    "url": "/BuildX-Admin-Panel/static/js/25.5dd8da3d.chunk.js"
+  },
+  {
+    "revision": "dae16a9f4d08e042cc7d",
+    "url": "/BuildX-Admin-Panel/static/js/26.6d106b64.chunk.js"
+  },
+  {
+    "revision": "0c3b4b407e4751a17d3b",
+    "url": "/BuildX-Admin-Panel/static/js/27.26f3cd10.chunk.js"
+  },
+  {
+    "revision": "36bbde384df68a7ec356",
+    "url": "/BuildX-Admin-Panel/static/js/28.3b12cd09.chunk.js"
+  },
+  {
+    "revision": "dd8add0b6a265ba8dcf0",
+    "url": "/BuildX-Admin-Panel/static/js/29.4563ac37.chunk.js"
+  },
+  {
+    "revision": "e7013e717d026d26ac6d",
+    "url": "/BuildX-Admin-Panel/static/js/3.1c9d54d2.chunk.js"
+  },
+  {
+    "revision": "44a044edc07ffb95a82853e75b87eb2b",
+    "url": "/BuildX-Admin-Panel/static/js/3.1c9d54d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86d9eb3d14a662370223",
+    "url": "/BuildX-Admin-Panel/static/js/30.2ece87bf.chunk.js"
+  },
+  {
+    "revision": "3193b93517d4f41db642",
+    "url": "/BuildX-Admin-Panel/static/js/31.51a63119.chunk.js"
+  },
+  {
+    "revision": "40e663e61d20fe97e27d",
+    "url": "/BuildX-Admin-Panel/static/js/32.9f57408e.chunk.js"
+  },
+  {
+    "revision": "852f6d5755cce1fe4395",
+    "url": "/BuildX-Admin-Panel/static/js/33.9f98fa19.chunk.js"
+  },
+  {
+    "revision": "78cba984bf00a2831521",
+    "url": "/BuildX-Admin-Panel/static/js/34.57d35e01.chunk.js"
+  },
+  {
+    "revision": "d81deae8b10e5d4fa059",
+    "url": "/BuildX-Admin-Panel/static/js/35.0cf92e2e.chunk.js"
+  },
+  {
+    "revision": "bfb6e2a87c093b6cb8c7",
+    "url": "/BuildX-Admin-Panel/static/js/36.b955024b.chunk.js"
+  },
+  {
+    "revision": "ca2cb92baba5b56fbeac",
+    "url": "/BuildX-Admin-Panel/static/js/37.5239500f.chunk.js"
+  },
+  {
+    "revision": "69a088fab2f6c082308c",
+    "url": "/BuildX-Admin-Panel/static/js/38.243b5f6a.chunk.js"
+  },
+  {
+    "revision": "b7e70446473110eae961",
+    "url": "/BuildX-Admin-Panel/static/js/39.e776929c.chunk.js"
+  },
+  {
+    "revision": "b833f21c3a6d20b5b3fc",
+    "url": "/BuildX-Admin-Panel/static/js/4.2f8030ea.chunk.js"
+  },
+  {
+    "revision": "87c4ec6e756a8a3779d6",
+    "url": "/BuildX-Admin-Panel/static/js/40.f4fb8350.chunk.js"
+  },
+  {
+    "revision": "11bdc6b289c2fec3df01",
+    "url": "/BuildX-Admin-Panel/static/js/41.1470cbbb.chunk.js"
+  },
+  {
+    "revision": "f59216b3dab969da6360",
+    "url": "/BuildX-Admin-Panel/static/js/42.6522be2e.chunk.js"
+  },
+  {
+    "revision": "14044bbe37d858cb09d91a37af7a413b",
+    "url": "/BuildX-Admin-Panel/static/js/42.6522be2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b14bf522e31a22acc31",
+    "url": "/BuildX-Admin-Panel/static/js/43.63152fab.chunk.js"
+  },
+  {
+    "revision": "772d387270142de9c7d5",
+    "url": "/BuildX-Admin-Panel/static/js/44.46c72e5b.chunk.js"
+  },
+  {
+    "revision": "ac64633c59eaad0d5285",
+    "url": "/BuildX-Admin-Panel/static/js/45.bd1ccd24.chunk.js"
+  },
+  {
+    "revision": "04e147b06d6e865ca22c",
+    "url": "/BuildX-Admin-Panel/static/js/46.2a0a74f3.chunk.js"
+  },
+  {
+    "revision": "626dda02b96b3a6d3bd2",
+    "url": "/BuildX-Admin-Panel/static/js/47.6e9dcd63.chunk.js"
+  },
+  {
+    "revision": "6e44acec3b4e7772fa59",
+    "url": "/BuildX-Admin-Panel/static/js/48.ff583c03.chunk.js"
+  },
+  {
+    "revision": "ce15c9d372cc5baeacdeb733817bdde4",
+    "url": "/BuildX-Admin-Panel/static/js/48.ff583c03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f55ee14b5e0f31cf0479",
+    "url": "/BuildX-Admin-Panel/static/js/49.26c4f2f8.chunk.js"
+  },
+  {
+    "revision": "e3cd93dacaec0683df6a",
+    "url": "/BuildX-Admin-Panel/static/js/5.e646817f.chunk.js"
+  },
+  {
+    "revision": "2ba40f737c972127bcfc",
+    "url": "/BuildX-Admin-Panel/static/js/50.d1cb332e.chunk.js"
+  },
+  {
+    "revision": "9ca4ed8d6699f93dfc82",
+    "url": "/BuildX-Admin-Panel/static/js/51.e00a3b87.chunk.js"
+  },
+  {
+    "revision": "6f6e5f91abbd0b3fc173",
+    "url": "/BuildX-Admin-Panel/static/js/52.c53123fd.chunk.js"
+  },
+  {
+    "revision": "9ac53079bd8edb195e48",
+    "url": "/BuildX-Admin-Panel/static/js/53.e3d8570a.chunk.js"
+  },
+  {
+    "revision": "3941f709451caa534b6f",
+    "url": "/BuildX-Admin-Panel/static/js/54.a45a4a22.chunk.js"
+  },
+  {
+    "revision": "abddd00d437007df26a5",
+    "url": "/BuildX-Admin-Panel/static/js/55.ad529358.chunk.js"
+  },
+  {
+    "revision": "828203f440161fcbca31",
+    "url": "/BuildX-Admin-Panel/static/js/56.f151a072.chunk.js"
+  },
+  {
+    "revision": "12647b5772736ef29ea2",
+    "url": "/BuildX-Admin-Panel/static/js/57.c782d9da.chunk.js"
+  },
+  {
+    "revision": "8748fccef772d7a18062",
+    "url": "/BuildX-Admin-Panel/static/js/58.e0ad09db.chunk.js"
+  },
+  {
+    "revision": "c92dbb300af18b75ee5c",
+    "url": "/BuildX-Admin-Panel/static/js/59.e4813585.chunk.js"
+  },
+  {
+    "revision": "241fcfac454287df9166",
+    "url": "/BuildX-Admin-Panel/static/js/6.cf2f7470.chunk.js"
+  },
+  {
+    "revision": "e4a0d87becbcc6d09668875fe197b50f",
+    "url": "/BuildX-Admin-Panel/static/js/6.cf2f7470.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3a1e883ea26abc47922",
+    "url": "/BuildX-Admin-Panel/static/js/60.1760e090.chunk.js"
+  },
+  {
+    "revision": "329a1648cc2fa248b43a",
+    "url": "/BuildX-Admin-Panel/static/js/61.9767578a.chunk.js"
+  },
+  {
+    "revision": "1ccc6f19b402056e65af",
+    "url": "/BuildX-Admin-Panel/static/js/62.a6c7fce4.chunk.js"
+  },
+  {
+    "revision": "2496a84e35a9eb6b2516",
+    "url": "/BuildX-Admin-Panel/static/js/63.179e816f.chunk.js"
+  },
+  {
+    "revision": "d3a390ceaaf04c2284c3",
+    "url": "/BuildX-Admin-Panel/static/js/64.5a33e7ac.chunk.js"
+  },
+  {
+    "revision": "ee4768b756178aea1bd3",
+    "url": "/BuildX-Admin-Panel/static/js/65.030197de.chunk.js"
+  },
+  {
+    "revision": "dfef7a2cd2f72171f83b",
+    "url": "/BuildX-Admin-Panel/static/js/66.2daf53c2.chunk.js"
+  },
+  {
+    "revision": "c5249c0bfe8b2c2a8228",
+    "url": "/BuildX-Admin-Panel/static/js/67.60d88138.chunk.js"
+  },
+  {
+    "revision": "8704b72421683f62a631",
+    "url": "/BuildX-Admin-Panel/static/js/68.1f4a291f.chunk.js"
+  },
+  {
+    "revision": "66fdad88d5ac382777e5",
+    "url": "/BuildX-Admin-Panel/static/js/69.f3515214.chunk.js"
+  },
+  {
+    "revision": "8ea6e2b41dd693644647",
+    "url": "/BuildX-Admin-Panel/static/js/7.f5f3289a.chunk.js"
+  },
+  {
+    "revision": "f9805473c6d3126eeae8",
+    "url": "/BuildX-Admin-Panel/static/js/70.0fc94316.chunk.js"
+  },
+  {
+    "revision": "c2761951624c88355855",
+    "url": "/BuildX-Admin-Panel/static/js/71.d8334d69.chunk.js"
+  },
+  {
+    "revision": "8644027fa44bfdf178fe",
+    "url": "/BuildX-Admin-Panel/static/js/72.a9106acc.chunk.js"
+  },
+  {
+    "revision": "85dd23010943c4530b52",
+    "url": "/BuildX-Admin-Panel/static/js/73.cfa25d77.chunk.js"
+  },
+  {
+    "revision": "3a724969a78ee2d6ac6d",
+    "url": "/BuildX-Admin-Panel/static/js/74.41acd87f.chunk.js"
+  },
+  {
+    "revision": "e4ac910a441d54955456",
+    "url": "/BuildX-Admin-Panel/static/js/75.bfb0a9ed.chunk.js"
+  },
+  {
+    "revision": "36804ae65f1d7e9656c7",
+    "url": "/BuildX-Admin-Panel/static/js/76.af682812.chunk.js"
+  },
+  {
+    "revision": "f220dbb0d1ec22343682",
+    "url": "/BuildX-Admin-Panel/static/js/77.a7c4e616.chunk.js"
+  },
+  {
+    "revision": "e4039acb17df8a51991b",
+    "url": "/BuildX-Admin-Panel/static/js/78.720a1fae.chunk.js"
+  },
+  {
+    "revision": "373cf7c15bb6e1e76c20",
+    "url": "/BuildX-Admin-Panel/static/js/79.8f26a1f2.chunk.js"
+  },
+  {
+    "revision": "e0c8125b29ad0491d416",
+    "url": "/BuildX-Admin-Panel/static/js/8.f09694bb.chunk.js"
+  },
+  {
+    "revision": "2dad5cd2b62bc95559a5",
+    "url": "/BuildX-Admin-Panel/static/js/80.219fec51.chunk.js"
+  },
+  {
+    "revision": "e104cddede98418cc1d6",
+    "url": "/BuildX-Admin-Panel/static/js/81.345d2441.chunk.js"
+  },
+  {
+    "revision": "0718663ec45b0e569067",
+    "url": "/BuildX-Admin-Panel/static/js/82.3dd4767b.chunk.js"
+  },
+  {
+    "revision": "fce550c5218473b90524",
+    "url": "/BuildX-Admin-Panel/static/js/83.8a725c36.chunk.js"
+  },
+  {
+    "revision": "9efaf25a7a8d6c0b0d92",
+    "url": "/BuildX-Admin-Panel/static/js/84.f5857bf7.chunk.js"
+  },
+  {
+    "revision": "613d2544c6f63fd63d9a",
+    "url": "/BuildX-Admin-Panel/static/js/85.fefbf568.chunk.js"
+  },
+  {
+    "revision": "54592536de9760dcfb83",
+    "url": "/BuildX-Admin-Panel/static/js/86.c098f57d.chunk.js"
+  },
+  {
+    "revision": "c74c4cd67539905243a2",
+    "url": "/BuildX-Admin-Panel/static/js/87.490436cd.chunk.js"
+  },
+  {
+    "revision": "eb18d6dca892d96b5e02",
+    "url": "/BuildX-Admin-Panel/static/js/9.094b10c2.chunk.js"
+  },
+  {
+    "revision": "e57ebb8ad1663e2d6cc5",
+    "url": "/BuildX-Admin-Panel/static/js/main.0310ec1d.chunk.js"
+  },
+  {
+    "revision": "7ca3fba5cc8b7e339378",
+    "url": "/BuildX-Admin-Panel/static/js/runtime-main.07aa79cf.js"
+  }
+]);
